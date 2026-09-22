d7a44d3d75b65e502473d3d8dfa2a056266ba6a0
Nwaje Emmanuel <emmanuelnwaje@gmail.com>
2026-09-23 00:04:43 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,53 +1,48 @@
-const CACHE_NAME =
-    "student-expense-tracker-v1";
+const CACHE_NAME = "student-expense-tracker-v2";
 
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./style.css",
     "./script.js",
-    "./manifest.json"
+    "./manifest.json",
+    "./icon.svg"
 ];
 
-
-self.addEventListener(
-    "install",
-    function(event) {
-
-        event.waitUntil(
-
-            caches.open(
-                CACHE_NAME
-            ).then(function(cache) {
-
-                return cache.addAll(
-                    FILES_TO_CACHE
-                );
-
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(FILES_TO_CACHE);
             })
+    );
 
-        );
+    self.skipWaiting();
+});
 
-    }
-);
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames
+                    .filter(function(name) {
+                        return name !== CACHE_NAME;
+                    })
+                    .map(function(name) {
+                        return caches.delete(name);
+                    })
+            );
+        })
+    );
 
+    self.clients.claim();
+});
 
-self.addEventListener(
-    "fetch",
-    function(event) {
-
-        event.respondWith(
-
-            caches.match(
-                event.request
-            ).then(function(response) {
-
-                return response ||
-                    fetch(event.request);
-
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
             })
-
-        );
-
-    }
-);
+    );
+});
